6cb643da66f4e65ab2c9e5d7574381cc20dad7ab
Konstantinos Tsalas <it0631@uom.edu.gr>
2017-05-10 15:57:36 +0300

    added up button,fixed accordion animation,fixed responsive issues

--- js/accordion.js
+++ js/accordion.js
@@ -1,22 +1,8 @@
 // JavaScript Document
 $(document).ready(function(){
 	'use strict';
-	/*$("#accordion h3").click(function(){
-		if ($(this).next().css("display") === "block"){
-			$(this).find("i").toggleClass("rotate");
-			$(this).next().css("display","none");
-			}
-		else{
-			if ($(this).parent().find("i").hasClass("rotate")){
-				$(this).parent().find("i").removeClass("rotate");
-				}
-			$(this).find("i").toggleClass("rotate");
-			$("#accordion div").css("display","none");
-			$(this).next().css("display","block");
-			}
-		
-		});*/
 		$("#accordion div").hide();
+		$("#accordion div").removeClass("display");
 		$("#accordion h3").click(function(){
 		if ($(this).next().hasClass("display")){
 			$(this).find("i").toggleClass("rotate");
@@ -28,6 +14,7 @@ $(document).ready(function(){
 				$(this).parent().find("i").removeClass("rotate");
 				}
 			$(this).find("i").toggleClass("rotate");
+			$("#accordion div").removeClass("display");
 			$("#accordion div").hide(400);
 			$(this).next().show(400);
 			$(this).next().addClass("display");
